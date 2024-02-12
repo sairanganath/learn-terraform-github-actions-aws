5a4c34582d0965a9799ce0e8bcd5a7942bbe8c0b
Sai Ranganath <s.b.sairanganath@gmail.com>
2024-02-12 20:31:02 +0530

    Use our Terraform Cloud organization For WellsOrganisation- added aws access id and access keys in apply.yml

---
+++ varibales.tf
@@ -0,0 +1,11 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type        = string
+  sensitive = true
+  description = "AWSID"
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type        = string
+  sensitive = true
+  description = "AWS KEY"
+}
