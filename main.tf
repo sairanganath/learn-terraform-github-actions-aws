c46a78884c00ae25b3507665f881f71d1d11fa99
Sai Ranganath <s.b.sairanganath@gmail.com>
2024-02-12 16:56:30 +0530

    Use our Terraform Cloud organization For WellsOrganisation- added aws access id and access keys in apply.yml

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  access_key = "${env}.AWS_ACCESS_KEY_ID"
-  secret_key = "${env}.AWS_SECRET_ACCESS_KEY"
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_SECRET_ACCESS_KEY
 }
 
 resource "random_pet" "sg" {}
